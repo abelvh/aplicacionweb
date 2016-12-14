66515636416113d4023f917ce410b5c818664469
abelvh <abelvh@gmail.com>
2016-12-13 21:28:06 -0500

    modificacion-11

--- before_install.sh
+++ before_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 yum -y install php70
 service httpd start
+mkdir /var/www/html/bzbackup
+cd /var/www/html
+chmod 755 -R bzbackup
+chown apache:apache -R bzbackup
 yum -y install mysql-server
 service mysqld start
 yum -y install php php-mysql
